12b49953387b363480e2610c798eab01d9c8103f
Karthikeyan A <karthikeyana.eee2021@citchennai.net>
2024-07-29 13:50:47 +0530

    error removed

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,60 @@
+"use client";
 import ClientAboutView from "@/components/client-view/about";
 import ClientContactView from "@/components/client-view/contact";
 import ClientExperienceAndEducationView from "@/components/client-view/experience";
 import ClientHomeView from "@/components/client-view/home";
 import ClientProjectView from "@/components/client-view/project";
+import { useState, useEffect } from "react";
 
-async function extractAllDatas(currentSection) {
-  const res = await fetch(
-    `http://karthiikportfolio.vercel.app/api/${currentSection}/get`,
-    {
+async function fetchData(currentSection) {
+  try {
+    const res = await fetch(`/api/${currentSection}/get`, {
       method: "GET",
       cache: "no-store",
-    }
-  );
+    });
 
-  const data = await res.json();
-
-  return data && data.data;
+    const data = await res.json();
+    return data && data.data;
+  } catch (error) {
+    console.error("Can't fetch data", error);
+    return null;
+  }
 }
 
-export default async function Home() {
-  const homeSectionData = await extractAllDatas("home");
-  const aboutSectionData = await extractAllDatas("about");
-  const experienceSectionData = await extractAllDatas("experience");
-  const educationSectionData = await extractAllDatas("education");
-  const projectSectionData = await extractAllDatas("project");
+export default function Home() {
+  const [homeSectionData, setHomeSectionData] = useState(null);
+  const [aboutSectionData, setAboutSectionData] = useState(null);
+  const [experienceSectionData, setExperienceSectionData] = useState(null);
+  const [educationSectionData, setEducationSectionData] = useState(null);
+  const [projectSectionData, setProjectSectionData] = useState(null);
+
+  useEffect(() => {
+    async function loadAllData() {
+      const homeData = await fetchData("home");
+      const aboutData = await fetchData("about");
+      const experienceData = await fetchData("experience");
+      const educationData = await fetchData("education");
+      const projectData = await fetchData("project");
+
+      setHomeSectionData(homeData);
+      setAboutSectionData(aboutData);
+      setExperienceSectionData(experienceData);
+      setEducationSectionData(educationData);
+      setProjectSectionData(projectData);
+    }
+
+    loadAllData();
+  }, []);
+
+  if (
+    !homeSectionData ||
+    !aboutSectionData ||
+    !experienceSectionData ||
+    !educationSectionData ||
+    !projectSectionData
+  ) {
+     <div>Loading...</div>;
+  }
 
   return (
     <div>
