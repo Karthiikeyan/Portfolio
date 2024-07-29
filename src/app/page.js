bb75ab02ab0fcb7b8b6681a95d00bc1fe47053f3
Karthikeyan A <karthikeyana.eee2021@citchennai.net>
2024-07-29 14:01:39 +0530

    new changes made

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ import { useState, useEffect } from "react";
 
 async function fetchData(currentSection) {
   try {
-    const res = await fetch(`/api/${currentSection}/get`, {
+    const res = await fetch(`http://localhost:3000/api/${currentSection}/get`, {
       method: "GET",
       cache: "no-store",
     });
@@ -53,7 +53,7 @@ export default function Home() {
     !educationSectionData ||
     !projectSectionData
   ) {
-     <div>Loading...</div>;
+    return <div>Loading...</div>;
   }
 
   return (
